c6f0b680b0778f5c86e454674d62e1bf05c78072
Simon Lau <simon.lau.sg@gmail.com>
2022-12-12 15:50:46 +0800

    add mongoose and mongo_uri

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
+console.log("Mongo_URI", MONGO_URI);
+mongoose.connect(MONGO_URI);
+
+app.use(morgan("dev"));
 app.use(express.static("../client/dist"));
 
 app.get("/", (req, res) => {
