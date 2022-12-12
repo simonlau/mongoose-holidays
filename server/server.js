21fdbc37f6ed8e6eab1fb77266021097ba0bc5a4
Simon Lau <simon.lau.sg@gmail.com>
2022-12-12 15:29:31 +0800

    Add express static for react

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(express.static("../client/dist"));
+
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World!" });
 });
